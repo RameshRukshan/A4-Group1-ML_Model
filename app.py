2f17b6cbff70ae304a3db7b22669ebdaaa11b7ba
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-12-21 11:36:57 +0530

    API

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+from tensorflow.keras.models import load_model
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+model = load_model('your_model_name.h5')  #Define model thet we need to run 
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    img_data = data['image_data']
+
+    # Preprocess the image
+    img = cv2.imread(img_data)
+    img = cv2.resize(img, (480, 360))
+    img = np.expand_dims(img, axis=0)
+
+    # Make a prediction
+    prediction = model.predict(img)
+
+    # Return the prediction as JSON
+    return jsonify(prediction=prediction.tolist())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3300)
